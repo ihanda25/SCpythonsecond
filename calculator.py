2c0294571306f90c33e7f47a07f6acf377aed55d
Ishaan Handa <ishaanhanda25@gmail.com>
2018-07-24 14:36:57 +0000

    timeconverter_calculator

--- calculator.py
+++ calculator.py
@@ -8,15 +8,18 @@ if operater == "plus" :
     answer = first_num + second_num
     time.sleep(0.5)
     print(answer)
+    print ("Created By Ishaan")
 else:
     if operater == "minus":
         time.sleep(0.5)
         print(first_num - second_num)
+        print ("Created By Ishaan")
     else:
         if operater == "multiply":
             answer = first_num * second_num
             time.sleep(0.5)
             print(answer)
+            print ("Created By Ishaan")
         else:
             if operater == "divide":
                 remainder = str(raw_input("Should I leave a remainder? "))
@@ -25,12 +28,16 @@ else:
                     remainder = first_num%second_num
                     time.sleep(0.5)
                     print(answer, " remainder ", remainder)
+                    print ("Created By Ishaan")
                 else:
                     answer = first_num / second_num
                     time.sleep(0.5)
                     print answer
+                    print ("Created By Ishaan")
                     
             else:
                 answer = first_num ** second_num
                 time.sleep(0.5)
                 print answer
+                print ("Created By Ishaan")
+                
